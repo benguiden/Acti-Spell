64d0850d0c8c20e13b1c5d6b305385b846531a11
Ben Guiden <benguiden@hotmail.co.uk>
2017-11-01 16:39:17 +0000

    Added end game state.

--- Assets/Scripts/Level Objects/CameraFollow.cs
+++ Assets/Scripts/Level Objects/CameraFollow.cs
@@ -13,6 +13,12 @@ public class CameraFollow: MonoBehaviour {
 
 	public PlayerController playerController;
 
+	public GameObject endGameObject;
+
+	public AnimationCurve endGameCurve;
+
+	public float endGameCurveTime;
+
 	private float yOffset;
 
 	private float targetY;
@@ -21,7 +27,10 @@ public class CameraFollow: MonoBehaviour {
 
 	private float lastLowestTargetY;
 
+	private bool follow;
+
 	void Start(){
+		follow = true;
 		targetY = this.transform.position.y;
 		if (target != null)
 			yOffset = this.transform.position.y - target.position.y;
@@ -30,24 +39,42 @@ public class CameraFollow: MonoBehaviour {
 	}
 
 	void Update () {
-		if (target != null) {
+		if ((target != null) && (follow)) {
 			targetY = target.position.y;
-			Vector3 pos = this.transform.position;
-			pos.y = targetY + yOffset;
-			Vector3 newPos = new Vector3();
-			if (playerController.IsGrounded ())
-				newPos = Vector3.Lerp (this.transform.position, pos, 1f - smoothness);
-			else
-				newPos = Vector3.Lerp (this.transform.position, pos, Mathf.Pow (1f - smoothness, 1.5f));
-			if (newPos.y > transform.position.y) {
-				transform.position = newPos;
-			} else {
-				transform.position = newPos;
-			}
 
 			//Check Fail
-			if (target.position.y < this.transform.position.y + failOffset)
-				UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+			if (target.position.y < this.transform.position.y + failOffset) {
+				follow = false;
+				target.gameObject.SetActive (false);
+				target = null;
+				StartCoroutine (EndGameUI ());
+			}
 		}
+
+		Vector3 pos = this.transform.position;
+		pos.y = targetY + yOffset;
+		Vector3 newPos = new Vector3();
+		if (playerController.IsGrounded ())
+			newPos = Vector3.Lerp (this.transform.position, pos, 1f - smoothness);
+		else
+			newPos = Vector3.Lerp (this.transform.position, pos, Mathf.Pow (1f - smoothness, 1.5f));
+		if (newPos.y > transform.position.y) {
+			transform.position = newPos;
+		} else {
+			transform.position = newPos;
+		}
+	}
+
+	private IEnumerator EndGameUI(){
+		endGameObject.SetActive (true);
+		float time = endGameCurveTime;
+		float scale;
+		while (time > 0f) {
+			scale = endGameCurve.Evaluate (1f - (time / endGameCurveTime));
+			endGameObject.transform.localScale = new Vector3 (scale, scale, 1f);
+			time -= Time.deltaTime;
+			yield return null;
+		}
+		endGameObject.transform.localScale = new Vector3 (1f, 1f, 1f);
 	}
 }
