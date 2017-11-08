0ec1f8f57962e0039a0cfe2c61f9c423d86e43bc
Saorwen <lynchsaoirse@gmail.com>
2017-11-08 13:55:16 +0000

    New music

--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -21,50 +21,36 @@ public class AudioManager : MonoBehaviour {
 		part4 = music [3];
 		level = Score.main.level;
 		part1.clip = musicParts[0];
+		part1.volume = 0.8f;
+		part2.volume = 0f;
+		part3.volume = 0f;
+		part4.volume = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		level = Score.main.level;
 		if ((level > 0 && level < 4) || (level > 4 && level < 8) || (level > 8)) {
-			if (part1.time > part1.clip.length - 0.1f) {
 				part2.clip = musicParts [1];
-				if (!part2.isPlaying) {
-					part2.Play ();
-				}
-			}
+				part2.volume = 0.8f;
 		}
 		else {
-			if (part2.time > part2.clip.length - 0.1f) {
-				part2.Stop ();
-			}
+			part2.volume = 0f;
 		}
 
 		if ((level > 1 && level < 4) || (level > 5 && level < 8) || (level > 9)) {
-			if (part1.time > part1.clip.length -0.1f) {
 				part3.clip = musicParts [2];
-				if (!part3.isPlaying) {
-					part3.Play ();
-				}
-			}
+				part3.volume = 0.8f;
 		}
 		else {
-				if (part3.time > part3.clip.length - 0.1f) {
-					part3.Stop ();
-				}
+			part3.volume = 0f;
 		}
 		if ((level > 2 && level < 4) || (level > 6 && level < 8) || (level > 10)) {
-			if (part1.time > part1.clip.length -0.1f) {
-				part4.clip = musicParts [3];
-				if (!part4.isPlaying) {
-					part4.Play ();
-				}
-			}
+			part4.clip = musicParts [3];
+			part4.volume = 0.8f;
 		}
 		else {
-				if (part4.time > part4.clip.length - 0.1f) {
-					part4.Stop ();
-				}
+			part4.volume = 0f;
 		}
 	}
 
