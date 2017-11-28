bcbd3fd95492a5b734257d836308329b833a8cb6
Ben Guiden <benguiden@hotmail.co.uk>
2017-11-28 15:48:36 +0000

    Added support for any aspect ratio.

---
+++ Assets/Scripts/UI/AspectUtility.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class AspectUtility : MonoBehaviour {
+
+	public float desiredRatio = 1.77777778f;
+	public float scaleConstant = 5;
+
+	private Camera cam;
+
+	private int lastWidth = 0;
+	private int lastHeight = 0;
+
+	private void Awake(){
+		cam = this.GetComponent<Camera> ();
+
+		CheckRatio ();
+
+	}
+
+	private void Update(){
+		if ((lastWidth != Screen.width) || (lastHeight != Screen.height)) {
+			lastWidth = Screen.width;
+			lastHeight = Screen.height;
+			CheckRatio ();
+		}
+	}
+
+	private void CheckRatio(){
+		float scaleAmount = desiredRatio / ((float)Screen.width / (float)Screen.height);
+		Camera.main.orthographicSize = scaleConstant * scaleAmount;
+	}
+
+}
