b69322b80a2ddaeecd0e5cbb7cbbc82648c32583
Ben Guiden <benguiden@hotmail.co.uk>
2017-12-11 17:14:02 +0000

    Finalizing game for submission.

--- Assets/Scripts/Game/HowToPlayControls.cs
+++ Assets/Scripts/Game/HowToPlayControls.cs
@@ -6,12 +6,22 @@ public class HowToPlayControls : MonoBehaviour {
 
 	public GameObject[] pages;
 
+	private AudioSource audioSource;
+
+	private void Awake(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
 	public void ChangeToPage(int pageIndex){
 		for (int i = 0; i < pages.Length; i++) {
 			if (i == pageIndex)
 				pages [i].SetActive (true);
 			else
 				pages [i].SetActive (false);
+		}
+		if ((audioSource != null) && (gameObject.activeSelf)) {
+			audioSource.time = 0f;
+			audioSource.Play ();
 		}
 	}
 
